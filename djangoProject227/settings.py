d1b55009aa981489c1a15c03773336c5c741a667
zhuxin888 <zhuhongxu1@yeah.net>
2023-02-27 16:57:29 +0300

    New updates for Heroku deployment

--- djangoProject227/settings.py
+++ djangoProject227/settings.py
@@ -117,3 +117,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+# pages_project/settings.py
+ALLOWED_HOSTS = ['*']git status
